0186fd9e2bde5d811947369246286adca9686444
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-03-24 22:09:43 +0530

    update slider of homepage

--- src/components/home/slider/Slider.jsx
+++ src/components/home/slider/Slider.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import styles from "./slider.module.css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
 import { sliderArr } from "../../../data/home/slider";
 
 export default function Slider() {
   return (
     <Swiper
       spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
+      slidesPerView={1}
+      modules={[Navigation, Autoplay]}
+      loop={true}
+      navigation
+      autoplay={{ delay: 3000 }}
+      onSlideChange={() => console.log("chaning")}
       onSwiper={swiper => console.log(swiper)}
       className={styles.swiper}
     >
@@ -17,7 +25,8 @@ export default function Slider() {
         <SwiperSlide key={index} className={styles.swiperSlide}>
           <div className={styles.leftContainer}>
             <p className={styles.title}>
-              {item.title}{" "}
+              {item.title}
+              <br />
               <span className={styles.span}>{item.highlightedText}</span>
             </p>
           </div>
