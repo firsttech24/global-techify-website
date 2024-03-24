5590898267f6f22d7e417210d5fa0581a04c085f
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-03-24 18:03:39 +0530

    profile forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+/** @format */
+
 import React, { useEffect } from "react";
 import Header from "./components/header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import MentorSignUp from "./pages/login/MentorSignup";
 
 export default function App() {
   useEffect(() => {
@@ -15,7 +18,14 @@ export default function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path={"/"} element={<HomePage />} />
+        <Route
+          path={"/"}
+          element={<HomePage />}
+        />
+        <Route
+          path="/mentor/signup"
+          element={<MentorSignUp />}
+        />
       </Routes>
     </BrowserRouter>
   );
