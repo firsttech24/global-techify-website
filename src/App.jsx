6bba3ebb2b085e1a936050db0b08f35f2bb2c29f
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-04-06 16:34:07 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,11 @@ import {
 } from "./pages";
 import MentorSignUp from "./pages/login/MentorSignup";
 import UserProfile from "./pages/login/UserProfile";
+import MentorDashboard from "./mentorPanel/mentorDashboard";
+import AcceptedRequests from "./mentorPanel/AcceptedRequests";
+import RequestPage from "./mentorPanel/requestPage";
+import UpcomingSessions from "./mentorPanel/UpcomingSessions";
+import CompletedSessions from "./mentorPanel/CompletedSessions";
 
 export default function App() {
   useEffect(() => {
@@ -67,6 +72,13 @@ export default function App() {
           path={"/mentor/profile"}
           element={<MentorSignUp />}
         />
+
+        <Route path="/mentor" element={<MentorDashboard/>}>
+            <Route path="meetrequests" element={<RequestPage/>} />
+            <Route path="acceptedrequests" element={<AcceptedRequests/>} />
+            <Route path="upcomingsessions" element={<UpcomingSessions/>} />
+            <Route path="completedsessions" element={<CompletedSessions/>} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/mentorPanel/UpcomingSessions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserCard from './components/UserCard'
+
+const UpcomingSessions = () => {
+    const buttons = [];
+  return (
+    <div>
+      <UserCard buttons={buttons}/>
+    </div>
+  )
+}
+
+export default UpcomingSessions
